6c9a12ea9132ee7c09c8818a885209af7e28c2ed
Alex van Engelen <alex@xorz.dev>
2021-01-11 16:37:07 +0100

    Init

---
+++ MeetupAPI/Entities/MeetupContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetupAPI.Entities
+{
+    public class MeetupContext : DbContext
+    {
+        public string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Database=MeetupDb";
+        public DbSet<Meetup> Meetups { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Meetup>()
+                .HasOne(m => m.Location)
+                .WithOne(l => l.Meetup)
+                .HasForeignKey<Location>(l => l.MeetupId);
+
+            modelBuilder.Entity<Meetup>()
+                .HasMany(m => m.Lectures)
+                .WithOne(l => l.Meetup);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
